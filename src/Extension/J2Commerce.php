57024e03dc2e870d35ea961d857c649c240341e1
Odin <webwizard@modernmagic.com>
2026-04-04 23:00:46 -0700

    Refactor J2Commerce plugin to implement SubscriberInterface

--- src/Extension/J2Commerce.php
+++ src/Extension/J2Commerce.php
@@ -1,38 +1,31 @@
 <?php
+defined('_JEXEC') or die;
 
-namespace ZOOlanders\YOOtheme\J2Commerce\Extension;
-
-use YOOtheme\Application;
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\CMS\Component\ComponentHelper;
+use Joomla\Event\SubscriberInterface;
 
-\defined('_JEXEC') or die;
-
-final class J2Commerce extends CMSPlugin
+class PlgSystemJ2storeYootheme extends CMSPlugin implements SubscriberInterface
 {
-    public function onAfterInitialise()
+    public static function getSubscribedEvents(): array
     {
-        // Check if the j2store component is enabled
-        if (!ComponentHelper::isEnabled('com_j2store', true)) {
+        return [
+            'onAfterInitialise' => 'boot',
+        ];
+    }
+
+    public function boot()
+    {
+        $app = \Joomla\CMS\Factory::getApplication();
+
+        if (!$app->isClient('site') && !$app->isClient('administrator')) {
             return;
         }
 
-        // Check if FOF is loaded
-        if (!defined('F0F_INCLUDED') && file_exists(JPATH_LIBRARIES . '/f0f/include.php')) {
-            include_once JPATH_LIBRARIES . '/f0f/include.php';
-        }
-
-        if (!defined('F0F_INCLUDED') || !class_exists('F0FLess', true)) {
+        if (!class_exists('\YOOtheme\Application')) {
             return;
         }
 
-        // Check if the YOOtheme app class exists
-        if (!class_exists(Application::class, false)) {
-            return;
-        }
-
-        // Load module from the same directory
-        $app = Application::getInstance();
-        $app->load(dirname(__DIR__) . '/Module/*/bootstrap.php');
+        $yooApp = \YOOtheme\Application::getInstance();
+        $yooApp->load(__DIR__ . '/src/Module/Source/bootstrap.php');
     }
 }
